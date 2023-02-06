335115c0666c6f9f39ac893b495ad4c9947b7ebc
Ibrahimova11 <tu7uncctx@code.edu.az>
2023-02-07 03:07:07 +0400

    Home page added section

--- client/src/Pages/Home/index.jsx
+++ client/src/Pages/Home/index.jsx
@@ -5,7 +5,44 @@ const Index = () => {
     <div>
     
     <Hotel/>
-
+    <section className="about top">
+    <div className="container">
+      <div className="aboutCard mtop flex_space">
+        <div className="row row1">
+          <h4>About Us</h4>
+          <h1>
+            We <span>provide Solution</span> to grow your buisness
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipiscing elit amet
+            consectetur piscing elit amet consectetur adipiscing elit sed et
+            eletum nulla eu placerat felis etiam tincidunt orci lacus id
+            varius dolor fermum sit amet.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipiscing elit amet
+            consectetur piscing elit amet consectetur adipiscing elit sed et
+            eletum nulla eu placerat felis etiam tincidunt orci lacus id
+            varius dolor fermum sit amet.
+          </p>
+          <button className="secondary-btn">
+            Explore More <i className="fas fa-long-arrow-alt-right"></i>
+          </button>
+        </div>
+        <div className="row image">
+          <img
+            src="https://rainbowdesign.in/themes/Stetho/img/about-us/about-video-img-1.jpg"
+            alt=""
+          />
+          <div className="control-btn">
+            <button className="prev">
+              <i className="fas fa-play"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 
       
     </div>
