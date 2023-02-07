c4975a9dc0b8549d15da56fb4289afcb3196e647
Ibrahimova11 <tu7uncctx@code.edu.az>
2023-02-08 03:35:41 +0400

    Detail page finished

--- client/src/Pages/Detail/index.jsx
+++ client/src/Pages/Detail/index.jsx
@@ -38,7 +38,45 @@ const Index = () => {
               <i className='fas fa-long-arrow-alt-left'></i> Go Back
             </Link>
 
-          
+            
+
+            <article className='content flex'>
+              <div className='main-content'>
+                <img src={item.image} alt='' />
+                <p>{item.desc}</p>
+                <p>{item.desc}</p>
+
+                <div className='para flex_space'>
+                  <p>{item.sidepara}</p>
+                  <p>{item.sidepara}</p>
+                </div>
+                <h1>Whta is the {item.title} City ?</h1>
+                <p>{item.desc}</p>
+
+                <div className='image grid1 '>
+                  <img src={item.paraImage_one} alt='' />
+                  <img src={item.paraImage_two} alt='' />
+                </div>
+                <p>{item.desc}</p>
+              </div>
+              
+
+              
+              <div className='side-content'>
+                <div className='box'>
+                  <h2>How can we help you?</h2>
+                  <p>{item.sidepara}</p>
+                  <button className='outline-btn'>
+                    <i className='fa fa-phone-alt'></i> Contact Us
+                  </button>
+                </div>
+
+                <div className='box2'>
+                  <p>{item.sidepara}</p>
+                </div>
+              </div>
+             
+            </article>
           </div>
         </section>
       ) : (
