c11b8c3f6e5dd36892dc304a4a9bb480d939dfdb
Ibrahimova11 <tu7uncctx@code.edu.az>
2023-02-08 03:33:55 +0400

    Detail page added go back button

--- client/src/Pages/Detail/index.jsx
+++ client/src/Pages/Detail/index.jsx
@@ -1,14 +1,22 @@
-import React  from "react"
+import React, { useState, useEffect } from "react"
 import { Link, useLocation } from "react-router-dom"
+import Ddata from "../Destinations/Ddata"
+import NotFound from "../../Components/NotFound"
+import { useParams } from "react-router-dom"
+
 import "./style.css"
 
-
-
-
 const Index = () => {
-    const location = useLocation();
+  const { id } = useParams()
+  const [item, setItem] = useState(null)
+  const location = useLocation();
 
-  
+  useEffect(() => {
+    let item = Ddata.find((item) => item.id === parseInt(id))
+    if (item) {
+      setItem(item)
+    }
+  }, [id])
 
   return (
     <>
@@ -23,7 +31,19 @@ const Index = () => {
     </div>
   </section>
 
-      
+      {item ? (
+        <section className='detail-page top'>
+          <div className='container'>
+            <Link to='/destinations' className='primary-btn back'>
+              <i className='fas fa-long-arrow-alt-left'></i> Go Back
+            </Link>
+
+          
+          </div>
+        </section>
+      ) : (
+        <NotFound />
+      )}
     </>
   )
 }
